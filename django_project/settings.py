38bc98be2fa07da1ffba945ad06def0da0bf3559
Lazik <temychin2011@yandex.ru>
2022-10-08 12:06:53 +0300

    test

--- django_project/settings.py
+++ django_project/settings.py
@@ -98,7 +98,7 @@ CELERY_TIMEZONE = TIME_ZONE
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
-CELERYBEAT_SCHEDULER = "djcelery.schedulers.DatabaseScheduler"
+CELERYBEAT_SCHEDULER = "django_celery_beat.schedulers.DatabaseScheduler"
 
 # CELERY_BROKER_URL = 'redis://:jpnmRrgJELWkwtYl@localhost:6379/0'
 # CELERY_RESULT_BACKEND = 'redis://:jpnmRrgJELWkwtYl@localhost:6379/0'
