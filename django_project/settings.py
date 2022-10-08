dd65144ec52801cf569b77d279e181fac2b9956e
Lazik <temychin2011@yandex.ru>
2022-10-08 12:02:28 +0300

    test

--- django_project/settings.py
+++ django_project/settings.py
@@ -100,10 +100,10 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 
 CELERYBEAT_SCHEDULER = "djcelery.schedulers.DatabaseScheduler"
 
-CELERY_BROKER_URL = 'redis://:jpnmRrgJELWkwtYl@localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://:jpnmRrgJELWkwtYl@localhost:6379/0'
-# CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
-# CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+# CELERY_BROKER_URL = 'redis://:jpnmRrgJELWkwtYl@localhost:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://:jpnmRrgJELWkwtYl@localhost:6379/0'
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
 
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
