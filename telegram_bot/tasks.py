99260c6b240566badf578e7d4a6ec6ccb2252a7c
Lazik <temychin2011@yandex.ru>
2022-10-09 02:28:35 +0300

    test

--- telegram_bot/tasks.py
+++ telegram_bot/tasks.py
@@ -44,7 +44,7 @@ def subscriptions_payment():
         # Дата для оплаты
         date_start = datetime.datetime.now() - timedelta(days=30)
 
-        for payment in Payment.objects.filter(date__lte=date_start, status=True):
+        for payment in Payment.objects.filter(date__gte=date_start, status=True):
             bot = telepot.Bot(payment.user.bot.token)
             # Делаем заголовок
             bot_text = f'Сообщения для Ильи\n\n{payment.id}'
