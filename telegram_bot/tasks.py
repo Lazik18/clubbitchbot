7273a853678f5f2d70f00b0f7e4f0eae9dbd285e
Lazik <temychin2011@yandex.ru>
2022-10-30 21:09:54 +0300

    test

--- telegram_bot/tasks.py
+++ telegram_bot/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+from celery import Celery
 
 from datetime import timedelta
 
@@ -8,7 +9,10 @@ import datetime
 
 from telegram_bot.models import TelegramBot, TelegramUser
 
-@shared_task
+app = Celery()
+
+
+@app.task
 def subscriptions():
     bot_settings = TelegramBot.objects.filter().first()
     bot = telepot.Bot(bot_settings.token)
@@ -16,33 +20,34 @@ def subscriptions():
     bot.sendMessage(chat_id='673616491', text='tes55t')
 
 
-@shared_task
+@app.task
 def accept_users():
     bot_settings = TelegramBot.objects.filter().first()
     bot = telebot.TeleBot(bot_settings.token)
-    
+
     # Выбрать пользователей, которые оплатили подписку
     users = TelegramUser.objects.exclude(subscription=None).filter(subscription__active=True)
-    
+
     # Пытаемся добавить этих пользователей в группу
     for user in users:
         try:
             bot.approve_chat_join_request(bot_settings.chat_id, user.chat_id)
         except Exception as e:
-            print(e)
+            pass
 
-@shared_task
+
+@app.task
 def remove_users():
     bot_settings = TelegramBot.objects.filter().first()
     bot = telebot.TeleBot(bot_settings.token)
-    
+
     # Выбрать пользователей у которых закончилась подписка
     users = TelegramUser.objects.excude(subscription=None).filter(subscription__active=False)
-    
+
     # Пытаемся удалить этих пользователей из группы
     for user in users:
         try:
             bot.ban_chat_member(bot_settings.chat_id, user.chat_id)
             bot.unban_chat_member(bot_settings.chat_id, user.chat_id)
         except Exception as e:
-            print(e)
+            pass
