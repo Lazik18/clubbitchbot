ccfd382247d92c5d6c5e77d24ef82662d3cdbc0e
Lazik <temychin2011@yandex.ru>
2022-10-30 21:10:12 +0300

    test

--- telegram_bot/tasks.py
+++ telegram_bot/tasks.py
@@ -19,35 +19,35 @@ def subscriptions():
 
     bot.sendMessage(chat_id='673616491', text='tes55t')
 
-
-@app.task
-def accept_users():
-    bot_settings = TelegramBot.objects.filter().first()
-    bot = telebot.TeleBot(bot_settings.token)
-
-    # Выбрать пользователей, которые оплатили подписку
-    users = TelegramUser.objects.exclude(subscription=None).filter(subscription__active=True)
-
-    # Пытаемся добавить этих пользователей в группу
-    for user in users:
-        try:
-            bot.approve_chat_join_request(bot_settings.chat_id, user.chat_id)
-        except Exception as e:
-            pass
-
-
-@app.task
-def remove_users():
-    bot_settings = TelegramBot.objects.filter().first()
-    bot = telebot.TeleBot(bot_settings.token)
-
-    # Выбрать пользователей у которых закончилась подписка
-    users = TelegramUser.objects.excude(subscription=None).filter(subscription__active=False)
-
-    # Пытаемся удалить этих пользователей из группы
-    for user in users:
-        try:
-            bot.ban_chat_member(bot_settings.chat_id, user.chat_id)
-            bot.unban_chat_member(bot_settings.chat_id, user.chat_id)
-        except Exception as e:
-            pass
+#
+# @app.task
+# def accept_users():
+#     bot_settings = TelegramBot.objects.filter().first()
+#     bot = telebot.TeleBot(bot_settings.token)
+#
+#     # Выбрать пользователей, которые оплатили подписку
+#     users = TelegramUser.objects.exclude(subscription=None).filter(subscription__active=True)
+#
+#     # Пытаемся добавить этих пользователей в группу
+#     for user in users:
+#         try:
+#             bot.approve_chat_join_request(bot_settings.chat_id, user.chat_id)
+#         except Exception as e:
+#             pass
+#
+#
+# @app.task
+# def remove_users():
+#     bot_settings = TelegramBot.objects.filter().first()
+#     bot = telebot.TeleBot(bot_settings.token)
+#
+#     # Выбрать пользователей у которых закончилась подписка
+#     users = TelegramUser.objects.excude(subscription=None).filter(subscription__active=False)
+#
+#     # Пытаемся удалить этих пользователей из группы
+#     for user in users:
+#         try:
+#             bot.ban_chat_member(bot_settings.chat_id, user.chat_id)
+#             bot.unban_chat_member(bot_settings.chat_id, user.chat_id)
+#         except Exception as e:
+#             pass
